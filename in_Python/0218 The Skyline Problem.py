17d3fddf4c5d083523a6372f720a648f8414f0cb
Yang Li <37635679+YangLiyli131@users.noreply.github.com>
2020-11-30 10:12:50 -0500

    Add files via upload

---
+++ in_Python/0218 The Skyline Problem.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def getSkyline(self, buildings):
+        """
+        :type buildings: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        events = [(L,-H,R) for L,R,H in buildings]
+        events += list({(R,0,0) for _,R,_ in buildings})
+        events.sort()
+        res = [[0,0]]
+        live = [(0, float('inf'))]
+        for pos, negH, R in events:
+            while live[0][1] <= pos:
+                heappop(live)
+            if negH:
+                heappush(live, (negH,R))
+            if res[-1][1] != -live[0][0]:
+                res += [[pos, -live[0][0]]]
+        return res[1:]
