145965813fd017e24b64de190b81b8cd361c4172
Yang Li <37635679+YangLiyli131@users.noreply.github.com>
2020-12-04 09:26:28 -0500

    Add files via upload

---
+++ in_Python/1492 The kth Factor of n.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def kthFactor(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        idx = 0
+        for i in range(1,n+1):
+            if n % i == 0:
+                idx += 1
+                if idx == k:
+                    return i
+        return -1
